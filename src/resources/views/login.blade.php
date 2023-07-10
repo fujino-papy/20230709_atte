034e739f0896b179f17a1756e5406412fcaa00b8
fujino-papy <papy41537@gmail.com>
2023-07-11 00:00:32 +0900

    20230710

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ログイン</title>
 </head>
 <body>
-    <h1>ログイン画面</h1>
+    <header>Atte</header>
+    <main>
+        <h1>ログイン</h1>
+        <div>
+            <input class="mail" type="email">
+        </div>
+        <div>
+            <input class="pass" type="password">
+        </div>
+        <div>
+            <button class="login_button" type="submit">ログイン</button>
+        </div>
+        <p class="guidance">アカウントをお持ちでない方はこちらから</p>
+        <a href="">会員登録</a>
+    </main>
+    <footer>
+        <p>Atte,inc.</p>
+    </footer>
 </body>
 </html>
